cb1d088d1fd8f39c90656d0af7dbb2e55a563de3
reAlpha39 <yunusw718@gmail.com>
2022-04-11 13:20:03 +0800

    change dummy page name

---
+++ lib/presentation/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorStyle {
+  static const Color cargooo = Color(0xFF64A1F4);
+  static const Color cartago_ = Color(0xFF3C7DD9);
+  static const Color redColor = Color(0xFFFF485A);
+}
